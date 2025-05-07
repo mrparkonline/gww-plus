06b420383790d37a8fcdc1904fadec507707e7b4
J Park <mrparkonline@gmail.com>
2025-05-07 09:33:57 -0400

    reinitializing project structure

---
+++ Home.py
@@ -0,0 +1,12 @@
+# GWW Plus - Home Page
+# List all clubs, sports, events, and volunteering
+
+# imports
+import streamlit as st
+
+# Streamlit App
+st.set_page_config(
+    page_title="Home"
+)
+
+st.write("Home Page")
---
+++ pages/Admin.py
@@ -0,0 +1,11 @@
+# GWW Plus - Admin Page
+
+# imports
+import streamlit as st
+
+# Streamlit App
+st.set_page_config(
+    page_title="Admin"
+)
+
+st.write("Admin Page")
