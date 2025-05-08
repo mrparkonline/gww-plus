873c095631fd38abb6599f7e3a374caa67e5a151
J Park <mrparkonline@gmail.com>
2025-05-08 07:58:39 -0400

    added some pages

---
+++ pages/Clubs.py
@@ -0,0 +1,11 @@
+# GWW Plus - Admin Page
+
+# imports
+import streamlit as st
+
+# Streamlit App
+st.set_page_config(
+    page_title="Clubs"
+)
+
+st.write("Clubs")
---
+++ pages/Sports.py
@@ -0,0 +1,11 @@
+# GWW Plus - Admin Page
+
+# imports
+import streamlit as st
+
+# Streamlit App
+st.set_page_config(
+    page_title="Sports"
+)
+
+st.write("Sports Page")
